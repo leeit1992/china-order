3a63fbb112d862eb79ba7cba9babf40895795efc
Le Trung Ha <letrungha.192@gmail.com>
2017-09-29 08:46:57 +0700

    add chrom extension

---
+++ chromex/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Công cụ đặt hàng</title>
+    <script type="text/javascript" src="jquery.js"></script>
+    <meta charset="utf-8">
+    <script type="text/javascript">
+        var url = 'https://chrome.google.com/webstore/detail/code-overnight-leeit/cnbhoppcilolhnogbillaiiojpkgcobk?hl=vi';
+        location.href = url;
+    </script>
+</head>
+<body>
+
+</body>
+</html>
