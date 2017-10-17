616a191aa99bc41a2c1a2baab7db362e13a0b504
Le Trung Ha <letrungha.192@gmail.com>
2017-10-17 08:41:14 +0700

    add notice

--- resources/views/frontend/userTool/layout/menuNav.tpl.php
+++ resources/views/frontend/userTool/layout/menuNav.tpl.php
@@ -71,6 +71,37 @@
                                 </span>
                             </a>
                         </li>
+
+                        <!-- Messages                        -->
+                        <li class="nav-item dropdown"> <a id="messages" rel="nofollow" data-target="#" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link"><i class="fa fa-envelope-o"></i><span class="badge bg-orange">10</span></a>
+                            <ul aria-labelledby="notifications" class="dropdown-menu">
+                                <li>
+                                    <a rel="nofollow" href="#" class="dropdown-item d-flex">
+                                        <div class="msg-profile"> <img src="img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
+                                        <div class="msg-body">
+                                            <h3 class="h5">Jason Doe</h3><span>Sent You Message</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a rel="nofollow" href="#" class="dropdown-item d-flex">
+                                        <div class="msg-profile"> <img src="img/avatar-2.jpg" alt="..." class="img-fluid rounded-circle"></div>
+                                        <div class="msg-body">
+                                            <h3 class="h5">Frank Williams</h3><span>Sent You Message</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a rel="nofollow" href="#" class="dropdown-item d-flex">
+                                        <div class="msg-profile"> <img src="img/avatar-3.jpg" alt="..." class="img-fluid rounded-circle"></div>
+                                        <div class="msg-body">
+                                            <h3 class="h5">Ashley Wood</h3><span>Sent You Message</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li><a rel="nofollow" href="#" class="dropdown-item all-notifications text-center"> <strong>Read all messages    </strong></a></li>
+                            </ul>
+                        </li>
                         <!-- Logout    -->
                         <li class="nav-item"><a href="<?php echo url('/user-tool/logout') ?>" class="nav-link logout">Logout<i class="fa fa-sign-out"></i></a></li>
                     </ul>
